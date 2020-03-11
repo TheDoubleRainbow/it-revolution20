5ea755aa09101766fde3d61d6cd52e7216d8f6fa
Andrew <16mb.com@gmail.com>
2020-03-12 00:45:00 +0200

    Login markup and start of home

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,18 +1,50 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header :username = "username"></Header>
+
+    <div class="home-container">
+      <div class="activity">
+        <div class="new-activity">
+          <div class="block-title">Add new activity</div>
+          <AddActivity />
+        </div>
+        <div class="activity-list">
+          <div class="block-title">Today activities</div>
+          <List />
+        </div>
+      </div>
+      <div class="graph">
+        <div class="block-title">Today activity satisfaction rate</div>
+        <Graph />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import Header from '../components/Header.vue';
+import AddActivity from '../components/AddActivity.vue';
+import Graph from '../components/Graph.vue';
+import List from '../components/List.vue';
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld,
+    Header,
+    AddActivity,
+    Graph,
+    List,
   },
+  data: () => ({
+    username: 'Username',
+  }),
 };
 </script>
+
+<style lang="scss" scoped>
+  .home-container {
+    max-width: 1170px;
+    margin: 0 auto;
+    display: flex;
+  }
+</style>
